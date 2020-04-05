8b120764629dfe05d761231a530b69f696859d10
Elozzy <st.eloghosa@gmail.com>
2020-04-05 11:50:46 +0100

    auth set

---
+++ auth.py
@@ -0,0 +1,18 @@
+#route to retrieve login  and signup page
+
+from flask import Blueprint
+from . import db
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return 'Login'
+
+@auth.route('/signup')
+def signup():
+    return 'Signup'
+
+@auth.route('/logout')
+def logout():
+    return 'Logout'
